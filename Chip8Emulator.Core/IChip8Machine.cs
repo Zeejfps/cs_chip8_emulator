474fdb68866b0b3ebe716cd9f9cce2f807652b4a
zeejfps <zeejfps@gmail.com>
2026-04-20 21:34:45 -0500

    Added some things to the interface for debugging purposes

--- Chip8Emulator.Core/IChip8Machine.cs
+++ Chip8Emulator.Core/IChip8Machine.cs
@@ -2,7 +2,9 @@ namespace Chip8Emulator.Core;
 
 public interface IChip8Machine
 {
+    ReadOnlySpan<byte> Memory { get; }
     IDisplay Display { get; }
+    public int ProgramCounter { get; }
     void LoadProgram(ReadOnlySpan<byte> program);
     void Update();
 }
