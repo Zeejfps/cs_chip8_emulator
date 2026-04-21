8c30789abd03bd465de5e56f322dabff29857b8d
zeejfps <zeejfps@gmail.com>
2026-04-20 21:14:17 -0500

    Added high res font

--- Chip8Emulator.Core/Impl/Chip8Machine.cs
+++ Chip8Emulator.Core/Impl/Chip8Machine.cs
@@ -4,9 +4,10 @@ namespace Chip8Emulator.Core.Impl;
 
 internal sealed class Chip8Machine : IChip8Machine
 {
-    private const int FontBaseAddress = 0x050;
+    private const int LowResFontBaseAddress = 0x050;
+    private const int HighResFontBaseAddress = 0x0A0;
 
-    private static ReadOnlySpan<byte> Font =>             
+    private static ReadOnlySpan<byte> LowResFont =>             
     [
         0xF0, 0x90, 0x90, 0x90, 0xF0, // 0                
         0x20, 0x60, 0x20, 0x20, 0x70, // 1                
@@ -24,6 +25,29 @@ internal sealed class Chip8Machine : IChip8Machine
         0xE0, 0x90, 0x90, 0x90, 0xE0, // D              
         0xF0, 0x80, 0xF0, 0x80, 0xF0, // E                
         0xF0, 0x80, 0xF0, 0x80, 0x80, // F              
+    ];
+
+    private static ReadOnlySpan<byte> HighResFont => [
+        // 0
+        0x3C, 0x7E, 0xE7, 0xC3, 0xC3, 0xC3, 0xC3, 0xE7, 0x7E, 0x3C,
+        // 1
+        0x18, 0x38, 0x58, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x3C,
+        // 2
+        0x3E, 0x7F, 0xC3, 0x06, 0x0C, 0x18, 0x30, 0x60, 0xFF, 0xFF,
+        // 3
+        0x3C, 0x7E, 0xC3, 0x03, 0x0E, 0x0E, 0x03, 0xC3, 0x7E, 0x3C,
+        // 4
+        0x06, 0x0E, 0x1E, 0x36, 0x66, 0xC6, 0xFF, 0xFF, 0x06, 0x06,
+        // 5
+        0xFF, 0xFF, 0xC0, 0xC0, 0xFC, 0xFE, 0x03, 0xC3, 0x7E, 0x3C,
+        // 6
+        0x3E, 0x7C, 0xC0, 0xC0, 0xFC, 0xFE, 0xC3, 0xC3, 0x7E, 0x3C,
+        // 7
+        0xFF, 0xFF, 0x03, 0x06, 0x0C, 0x18, 0x30, 0x60, 0x60, 0x60,
+        // 8
+        0x3C, 0x7E, 0xC3, 0xC3, 0x7E, 0x7E, 0xC3, 0xC3, 0x7E, 0x3C,
+        // 9
+        0x3C, 0x7E, 0xC3, 0xC3, 0x7F, 0x3F, 0x03, 0x03, 0x3E, 0x7C
     ];
     
     private const int DisplayWidth = 64;
@@ -65,7 +89,8 @@ internal sealed class Chip8Machine : IChip8Machine
         _input = input;
         _ticksPerFrame = clock.Frequency / 60;
         _lastTimestamp = clock.Timestamp;
-        Font.CopyTo(_memory.AsSpan(FontBaseAddress));
+        LowResFont.CopyTo(_memory.AsSpan(LowResFontBaseAddress));
+        HighResFont.CopyTo(_memory.AsSpan(HighResFontBaseAddress));
     }
 
     public int ProgramCounter => _programCounter;
@@ -113,7 +138,7 @@ internal sealed class Chip8Machine : IChip8Machine
     public void LoadProgram(ReadOnlySpan<byte> program)
     {
         Array.Clear(_memory);
-        Font.CopyTo(_memory.AsSpan(FontBaseAddress));
+        LowResFont.CopyTo(_memory.AsSpan(LowResFontBaseAddress));
         program.CopyTo(_memory.AsSpan(0x200));
         _programCounter = 0x200;
         _indexRegister = 0;
@@ -382,7 +407,7 @@ internal sealed class Chip8Machine : IChip8Machine
     {
         var x = ExtractX(ins);
         var value = _vRegisters[x];
-        _indexRegister = value * 5 + FontBaseAddress;
+        _indexRegister = value * 5 + LowResFontBaseAddress;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
